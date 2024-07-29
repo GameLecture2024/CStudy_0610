929d4410bca136a8b3dd30ecf5342cf311196d8f
GameLecture2024 <dlfjswodlf@naver.com>
2024-07-29 17:53:56 +0900

    (2024-07-29) 몬스터 데이터 프로그램 구현

--- MonsterDataManager/main.c
+++ MonsterDataManager/main.c
@@ -1,23 +1,12 @@
 #include <stdio.h>
 #include <string.h>
-
-typedef struct Monster {
-	char name[30];
-	char region[30];
-	char grade[30];
-	int level;
-}Monster;
+#include "Function.h"
 
 // 1. Monster name, region, grade, level 구조체 변수를 선언해서 printf 출력.
 // 2. 구조체 배열. 만들어 보고 반복문으로 몬스터 배열 출력.
 // 3. 배열에다가 구조체 배열 데이터를 넣어야 겠죠.
 // 4. char name[100][30] = Monster.name;
 
-int AddMonster(char(*monster_name)[30], char(*monster_region)[30], char(*monster_grade)[30],
-	int level, int* totalCount, Monster* monsterPtr)
-{
-
-}
 
 int SearchMonster();	 // compare 함수 만들어야 합니다.
 int ShowAllMonster();	 // printf( 배열에 있는 모든 요소 Monster[i].name)...
@@ -27,16 +16,17 @@ int main()
 {
 	int playerInput = 0;
 
+	Monster monsterGroup[100]; // 몬스터의 정보 100개를 저장하는 Monster 구조체
+
 	char monster_name[100][30];
 	char monster_region[100][30];
 	char monster_grade[100][30];
 	int monster_level[100];
 
-	int totalMonsterCount; // 몬스터 데이터에 몇번 Index에 저장되었는가
+	int totalMonsterCount = 0; // 몬스터 데이터에 몇번 Index에 저장되었는가
 
 	while (1)
 	{
-		system("cls");
 		printf("1. 몬스터 데이터 추가\n");
 
 		// 1. 몬스터 데이터 추가
@@ -46,6 +36,15 @@ int main()
 		printf("4. 프로그램 종료\n");
 
 		scanf_s("%d", &playerInput);
+
+		if (playerInput == 1) {
+			AddMonsterInfo(monsterGroup, &totalMonsterCount);
+		}
+
+		if (playerInput == 2) {
+			// SearchByName();
+			printf("몬스터 이름 : %s, 몬스터 지역 : %s, 몬스터 등급 : %s", monsterGroup[0].name, monsterGroup[0].region, monsterGroup[0].grade);
+		}
 
 		if (playerInput == 4) {										
 			break;
